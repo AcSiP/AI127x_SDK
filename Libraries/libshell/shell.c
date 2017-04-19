9e9085a4ff91072ac9ba0e0651347d4ad22a58a4
Fox @ M4800 <shenfa@acsip.com.tw>
2017-04-19 14:04:52 +0800

    Coding style.

--- Libraries/libshell/shell.c
+++ Libraries/libshell/shell.c
@@ -19,113 +19,101 @@
 
 #include "shell.h"
 
-int shell_str_len(char *str)
+int	shell_str_len( const char *str )
 {
-     int i = 0;
+	int	i = 0;
 
-     while(str[i++] != 0);
-
-     return i - 1;
+	while( str[i++] ) {
+	}
+	return i - 1;
 }
 
-int shell_str_cmp(char *str1, char *str2, int len1, int len2)
+int	shell_str_cmp( const char *str1, const char *str2, int len1, int len2 )
 {
-     int i;
+	int	i;
 
-     if(len1 > len2) return 1;
+	if( len1 > len2 ) return 1;
 
-     for(i = 0; i < len1; i++) {
-          if(str1[i] != str2[i]) return 2;
-     }
+	for( i = 0 ; i < len1 ; i++ ) {
+		if( str1[i] != str2[i] ) return 2;
+	}
 
-     // make sure we matched a whole command, and not only
-     // a containing substring ...
-     if(len2 > len1 && str2[i] != ' ') {
-          return 2;
-     }
+	// make sure we matched a whole command, and not only a containing substring ...
+	if( len2 > len1 && str2[i] != ' ' ) return 2;
 
-     return 0;
+	return 0;
 }
 
-int shell_parse_int(char *str)
+int	shell_parse_int( const char *str )
 {
+	int	val = 0;
+	int	i   = 0;
+	char	c;
 
-     int val = 0;
-     int i   = 0;
+	while( (c = str[i++]) != 0 ) {
+		if(c < '0' || c > '9') break;
+		val = val * 10 + (c - '0');
+	}
 
-     char c;
-
-     while((c = str[i++]) != 0) {
-
-          if(c < '0' || c > '9') break;
-
-          val = val * 10 + (c - '0');
-     }
-
-     return val;
+	return val;
 }
 
-int shell_arg_parser(char *cmd_line, int len,  shell_cmd_args *args)
+int	shell_arg_parser( const char *cmd_line, int len,  shell_cmd_args *args )
 {
-     int i;
-     int j;
-     int spos = 0;
-     int argc = 0;
+	int	i;
+	int	j;
+	int	spos = 0;
+	int	argc = 0;
 
-     for(i = 0; i < len; i++) {
-          // to many arguments
-          if(argc > SHELL_MAX_ARGS) return 1;
+	for( i = 0; i < len; i++ ) {
+		// to many arguments
+		if(argc > SHELL_MAX_ARGS) return 1;
 
-          if(cmd_line[i] == ' ' || i == len - 1) {
-               // catch last argument ...
-               if(i == len - 1) i++;
+		if( cmd_line[i] == ' ' || i == len - 1 ) {
+			// catch last argument ...
+			if( i == len - 1 ) i++;
 
-               // ignore first since it is the cmd itself
-               if(spos == 0) {
-                    spos = i;
-               } else {
-                    // argument value to long
-                    if(i - spos > SHELL_MAX_ARG_LEN) return 2;
+			// ignore first since it is the cmd itself
+			if( spos == 0 ) {
+				spos = i;
+			} else {
+				// argument value to long
+				if(i - spos > SHELL_MAX_ARG_LEN) return 2;
 
-                    for(j = 0; j < i - spos - 1; j++) {
-                         args->args[argc].val[j] = cmd_line[spos + 1 + j];
-                    }
-                    args->args[argc++].val[j] = 0;
-                    spos = i;
-               }
-          }
-     }
+				for( j = 0; j < i - spos - 1; j++ ) {
+					args->args[argc].val[j] = cmd_line[spos + 1 + j];
+				}
+				args->args[argc++].val[j] = 0;
+				spos = i;
+			}
+		}
+	}
 
-     args->count = argc;
-
-     return 0;
+	args->count = argc;
+	return 0;
 }
 
-int shell_process_cmds(shell_cmds *cmds, char *cmd_line)
+
+int	shell_process_cmds( const shell_cmd *cmd, unsigned char cmds, const char *cmd_line )
 {
-     int i;
-     int ret;
-     int cmd_len;
-     int cmd_line_len;
+	int		i;
+	int		ret;
+	int		cmd_len;
+	int		cmd_line_len;
+	shell_cmd_args	args;
 
-     shell_cmd_args args;
+	for( i = 0; i < cmds; i++ ) {
+		cmd_line_len	= shell_str_len( cmd_line );
+		cmd_len = shell_str_len( cmd[i].cmd );
 
-     for(i = 0; i < cmds->count; i++) {
+		if( shell_str_cmp( cmd[i].cmd, cmd_line, cmd_len, cmd_line_len ) == 0 ) {
+			ret = shell_arg_parser( cmd_line, cmd_line_len, &args );
 
-          cmd_line_len 	= shell_str_len(cmd_line);
-          cmd_len 		= shell_str_len((char *)(cmds->cmds[i].cmd));
+			if( ret == 1 ) return SHELL_PROCESS_ERR_ARGS_MAX;
+			if( ret == 2 ) return SHELL_PROCESS_ERR_ARGS_LEN;
 
-          if(shell_str_cmp((char *)(cmds->cmds[i].cmd), cmd_line, cmd_len, cmd_line_len) == 0) {
-               ret = shell_arg_parser(cmd_line, cmd_line_len, &args);
-
-               if(ret == 1)
-                    return SHELL_PROCESS_ERR_ARGS_MAX;
-               if(ret == 2)
-                    return SHELL_PROCESS_ERR_ARGS_LEN;
-
-               return (cmds->cmds[i].func)(&args);
-          }
-     }
-
-     return SHELL_PROCESS_ERR_CMD_UNKN;
+			return ( cmd[i].func( & args ) );
+		}
+	}
+	return SHELL_PROCESS_ERR_CMD_UNKN;
 }
--- Libraries/libshell/shell.h
+++ Libraries/libshell/shell.h
@@ -17,104 +17,59 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef __SHELL_H_
-#define __SHELL_H_
+#ifndef LIBRARIES_LIBSHELL_SHELL_H_
+#define LIBRARIES_LIBSHELL_SHELL_H_
 
 #include "radio.h"
 
-/**
- * max number of character for a command line passed to the shell
- */
-#define SHELL_MAX_CMD_LINE	90	
+// max number of character for a command line passed to the shell
+#define SHELL_MAX_CMD_LINE		90
 
-/**
- * max number of arguments passed with one command to the shell
- */
-#define SHELL_MAX_ARGS 		 3
+// max number of arguments passed with one command to the shell
+#define SHELL_MAX_ARGS			3
 
-/**
- * max number of character for a single argument form a command line passed to the shell
- */
-#define SHELL_MAX_ARG_LEN	MAX_LoraPayloadLength
+// max number of character for a single argument form a command line passed to the shell
+#define SHELL_MAX_ARG_LEN		MAX_LoraPayloadLength
 
-/**
- * return code given when processing of a command line was OK
- */
-#define SHELL_PROCESS_OK	 0
+// return code given when processing of a command line was OK
+#define SHELL_PROCESS_OK		0
 
-/**
- * ERROR maximum number of arguments was reached
- */
+// ERROR maximum number of arguments was reached
 #define SHELL_PROCESS_ERR_ARGS_MAX	0xfff0
 
-/**
- * ERROR maximum number of chars for an argument was reached
- */
+// ERROR maximum number of chars for an argument was reached
 #define SHELL_PROCESS_ERR_ARGS_LEN	0xfff1
 
-/**
- * ERROR unknown command
- */
-#define SHELL_PROCESS_ERR_CMD_UNKN 0xfff2
+// ERROR unknown command
+#define SHELL_PROCESS_ERR_CMD_UNKN	0xfff2
 
-/**
- * Single command argument
- */
+
+// Single command argument
 typedef struct {
-     /**
-      * Value representing the argument
-      */
-     char 			val[SHELL_MAX_ARG_LEN];
+	// Value representing the argument
+	char 		val[ SHELL_MAX_ARG_LEN ];
 } shell_cmd_arg;
 
-/**
- * All arguments from a single command line
- */
+// All arguments from a single command line
 typedef struct {
-     /**
-      * Number of arguments
-      */
-     unsigned char	count;
-
-     /**
-      * The arguments
-      */
-     shell_cmd_arg	args[SHELL_MAX_ARGS];
+	unsigned char	count;				// Number of arguments
+	shell_cmd_arg	args[SHELL_MAX_ARGS];		// The arguments
 } shell_cmd_args;
 
-/**
- * Definition of a single shell command
- */
+
+// Definition of a single shell command
 typedef struct {
-     /**
-      * Name of the command
-      */
-     const char 	*cmd;
+	const char 	*cmd;		// Name of the command
+	const char	*desc;		// Description of the command
 
-     /**
-      * Description of the command
-      */
-     const char		*desc;
-
-     /**
-      * Functino called when executing the commmand
-      */
-     int (*func)(shell_cmd_args *args);
+	int (*func)(shell_cmd_args *args);		// Functino called when executing the commmand
 } shell_cmd;
 
-/**
- * All shell commands knwon by the shell
- */
-typedef struct {
-     /**
-      * Number of commands
-      */
-     unsigned char		count;
 
-     /**
-      * The commands
-      */
-     shell_cmd			cmds[];
+// All shell commands knwon by the shell
+typedef struct {
+	unsigned char	count;		// Number of commands
+	shell_cmd	*cmds;		// The commands
 } shell_cmds;
 
 /**
@@ -123,7 +78,7 @@ typedef struct {
  * @param[in]	str	string for which to calculate the length
  * @return		length of str
  */
-int shell_str_len(char *str);
+int	shell_str_len( const char *str );
 
 /**
  * Comapre str1 with given length len1 to str2 with given length len2.
@@ -134,7 +89,7 @@ int shell_str_len(char *str);
  * @param[in] 	len2	length of second string
  * @return	0 if str1 euqals str2, 1 if len1 != len2, 2 if str1 != str2
  */
-int shell_str_cmp(char *str1, char *str2, int len1, int len2);
+int	shell_str_cmp( const char *str1, const char *str2, int len1, int len2 );
 
 /**
  * Parse the integer value from str and return it.
@@ -142,9 +97,9 @@ int shell_str_cmp(char *str1, char *str2, int len1, int len2);
  * @param[in]	str from which to parse the integer value.
  * @return	the integer value of str
  */
-int shell_parse_int(char *str);
+int	shell_parse_int( const char *str );
 
-int shell_arg_parser(char *cmd_line, int len,  shell_cmd_args *args);
+int	shell_arg_parser( const char *cmd_line, int len,  shell_cmd_args *args );
 
 /**
  * Process a command line string given in cmd_line against the
@@ -162,7 +117,8 @@ int shell_arg_parser(char *cmd_line, int len,  shell_cmd_args *args);
  * 			SHELL_PROCESS_ERR_ARGS_LEN if an argument string was too long,
  * 			SHELL_PROCESS_ERR_CMD_UNK if the command was unknown
  */
-int shell_process_cmds(shell_cmds *cmds, char *cmd_line);
+//int	shell_process_cmds( shell_cmds *cmds, const char *cmd_line );
+int	shell_process_cmds( const shell_cmd *cmd, unsigned char cmds, const char *cmd_line );
 
 /**
  * Process a command line. For details see {@link shell_process_cmds}.
@@ -172,6 +128,6 @@ int shell_process_cmds(shell_cmds *cmds, char *cmd_line);
  * @param[in]	*cmd_line see {@link shell_process_cmds}
  * @return 	see {@link shell_process_cmds}
  */
-//int shell_process(char *cmd_line);
+// int		shell_process( char *cmd_line );
 
-#endif
+#endif		// LIBRARIES_LIBSHELL_SHELL_H_
