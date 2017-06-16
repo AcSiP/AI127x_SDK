83e50da713595d64edf0bfa18e77428028155b8b
Fox@VM <shenfa@acsip.com.tw>
2017-06-16 17:29:26 +0800

    Update version string with '2.12.2017.0617'.

--- USER/config.h
+++ USER/config.h
@@ -1,4 +1,4 @@
-﻿
+
 //---------------------------------------------------------------------------
 /*
 //==========================================
@@ -29,7 +29,7 @@
 
 
 /* Exported define -----------------------------------------------------------*/
-#define FirmwareVersion				("1.12.2017.0610")
+#define FirmwareVersion				("2.12.2017.0617")
 #define ComPortBaudRate				(38400)
 #define GPSnoLocated_RunningTime		(60)		// Unit:s
 #define	DEF_Allowed_LoRa_Rx_Failure_Times_Times	(5)		// Unit: polling round
@@ -72,8 +72,12 @@
 
 
 #define MaxPacketSize				(255)
-#define MaxMsgDataSize				(244)		// 32(Total Size = MaxPacketSize) - 9(Length:1、Flag:1、SN:1、NodeAdd:3、NextHoppingChannel:1、CRC:2) - 3(MyAdd) = 20
-								// 在預設的一個訊框 32bytes 長度,一個 LoraNodeData 的訊框可以放進 24bytes 的 BASE64 的資料長度。
+/* Fix by JC in 20170616 */
+//#define MaxMsgDataSize				(243)		// 255(Total Size = MaxPacketSize) - 9(Length:1、Flag:1、SN:1、NodeAdd:3、NextHoppingChannel:1、CRC:2) - 3(MyAdd) = 243
+								// 在預設的一個訊框 255bytes 長度,一個 LoraNodeData 的訊框可以放進 243bytes 的 BASE64 的資料長度。
+
+#define MaxMsgDataSize				(246)		// 255(Total Size = MaxPacketSize) - 6(Length:1、Flag:1、NodeAdd:3、NextHoppingChannel:1) - 3(MyAdd) = 246
+/* Fix end */
 
 /* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
